609d138349a508ed84302b793ec26cbb03529c3c
Nenad Miražić <nenadmirazic@hotmail.com>
2023-09-01 00:22:18 +0200

    Fix dist files being created in src dir

--- 03-back-end/src/components/administrator/dist/AdministratorModel.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.AdministratorModel = void 0;
-var AdministratorModel = /** @class */ (function () {
-    function AdministratorModel() {
-    }
-    return AdministratorModel;
-}());
-exports.AdministratorModel = AdministratorModel;
--- 03-back-end/src/dist/routers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var AdministratorRouter_router_1 = require("./components/administrator/AdministratorRouter.router");
-var CategoryRouter_router_1 = require("./components/category/CategoryRouter.router");
-var AplicationRouters = [
-    new CategoryRouter_router_1["default"](),
-    new AdministratorRouter_router_1["default"](),
-];
-exports["default"] = AplicationRouters;
