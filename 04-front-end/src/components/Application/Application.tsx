ef6b9b9ba7866e530d9aff1c61a0d37bbee5c688
runja87 <runja87@gmail.com>
2023-10-09 21:37:33 +0200

    Fixup: issue with node-sass

--- 04-front-end/src/components/Application/Application.tsx
+++ 04-front-end/src/components/Application/Application.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Application.sass';
+import './Application.scss';
 
 function Application() {
   return (
--- 04-front-end/src/index.tsx
+++ 04-front-end/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.sass';
+import './index.scss';
 import Application from './components/Application/Application';
 import reportWebVitals from './common/reportWebVitals';
 
