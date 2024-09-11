22c4bf8c0c453e1493036ebdeef63004182f8c38
runja87 <runja87@gmail.com>
2024-09-11 14:53:26 +0200

    Update routers

--- 03-back-end/src/routers.ts
+++ 03-back-end/src/routers.ts
@@ -3,6 +3,7 @@ import AuthRouter from './components/auth/AuthRouter.router';
 import CategoryRouter from './components/category/CategoryRouter.router';
 import ContactRouter from './components/contact/ContactRouter.router';
 import PageRouter from './components/page/PageRouter.router';
+import PhotoRouter from './components/photo/PhotoRouter.router';
 
 const AplicationRouters = [
     new CategoryRouter(),
@@ -10,5 +11,6 @@ const AplicationRouters = [
     new PageRouter(),
     new ContactRouter(),
     new AuthRouter(),
+    new PhotoRouter()
 ];
 export default AplicationRouters;
