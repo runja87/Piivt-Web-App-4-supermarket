f6e29d135fb71bdedda1e08f9da5d590c2cc974c
runja87 <runja87@gmail.com>
2024-10-30 19:20:01 +0100

    Implement Frontend configs

---
+++ 04-front-end/src/common/IConfig.interface.ts
@@ -0,0 +1,6 @@
+export default interface IConfig {
+    domain: {
+        name: string,
+        port: number
+    }
+}
---
+++ 04-front-end/src/configs.ts
@@ -0,0 +1,11 @@
+import IConfig from "./common/IConfig.interface"
+const DevConfig: IConfig = {
+    domain: {
+        name: "http://localhost",
+        port: 10000
+    },
+
+}
+
+export default DevConfig;
+
