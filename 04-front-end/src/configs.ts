e7ce61a80d4ab159c0e88bf65b457b90514687cf
Nenad Miražić <nenadmirazic@hotmail.com>
2025-09-25 16:30:30 +0200

    Update Front End config.ts to use .env

--- 04-front-end/src/configs.ts
+++ 04-front-end/src/configs.ts
@@ -1,22 +1,21 @@
-import IConfig from "./common/IConfig.interface";
 import AuthStore from "./stores/AuthStore";
+import IConfig from "./common/IConfig.interface";
 
 const DevConfig: IConfig = {
-    domain: {
-        name: "http://localhost",
-        port: 10000,
+  domain: {
+    name: process.env.REACT_APP_API_DOMAIN || "http://localhost",
+    port: Number(process.env.REACT_APP_API_PORT || 10000),
+  },
+  authorization: {
+    get token() {
+      return `Bearer ${AuthStore.getState().authToken}`;
     },
-    authorization: {
-        get token() {
-            return `Bearer ${AuthStore.getState().authToken}`;
-        },
+  },
+  refresh: {
+    get token() {
+      return `Bearer ${AuthStore.getState().refreshToken}`;
     },
-    refresh: {
-        get token() {
-            return `Bearer ${AuthStore.getState().refreshToken}`;
-        },
-    },
+  },
 };
 
 export default DevConfig;
-
